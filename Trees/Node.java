09f934eb7f91c4cb9a91d16c3f3e7c6bb58911c0
WuGambinos <69660261+WuGambinos@users.noreply.github.com>
2021-08-18 12:46:17 -0400

    Added BST DFS code

---
+++ Trees/Node.java
@@ -0,0 +1,15 @@
+package Trees;
+
+/* Class containing left and right child of
+current node and key value*/
+class Node
+{
+	int data;
+	Node left, right;
+
+	public Node(int item)
+	{
+		data = item;
+		left = right = null;
+	}
+}
